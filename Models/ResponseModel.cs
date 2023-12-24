6fc7652583b8fd5e921e69296f4b2241840415c3
MubashirMemon <57357842+MubashirMemon@users.noreply.github.com>
2023-12-24 10:48:54 +0500

    ASP.NET WebAPI Weather App

---
+++ Models/ResponseModel.cs
@@ -0,0 +1,35 @@
+﻿using WeatherApp.Models;
+
+namespace WeatherApp.Models
+{
+    public class ResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public String? Message { get; set; }
+        public dynamic? JsonData { get; set; }
+    
+    public static ResponseModel Success(dynamic jsonData)
+    {
+        return new ResponseModel
+        {
+            JsonData = jsonData,
+            Message = null,
+            IsSuccess = true
+        };
+    }
+    public static ResponseModel Error(string errorMessage)
+    {
+        return new ResponseModel
+        {
+            JsonData = null,
+            Message = errorMessage,
+            IsSuccess = false
+                };
+              }
+
+        internal static ResponseModel Error(ResponseModel errorResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+        }
---
+++ Services/WeatherService.cs
@@ -0,0 +1,35 @@
+﻿using Flurl.Http;
+using System.Linq.Expressions;
+
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class WeatherService
+    {
+        public async Task<ResponseModel> GetWeatherData(string location)
+        {
+            string weatherApiUrl = AppConstants.WeatherApiUrl;
+            string weatherApiKey = AppConstants.WeatherApiKey;
+            string apiUrl = $"{weatherApiUrl}?access_key={weatherApiKey}&query=42.3605, -71.059";
+            try
+            {
+                var response = await apiUrl.GetAsync();
+                if (response.StatusCode == 200)
+                {
+                    var responseData = await response.GetJsonAsync<WeatherModel>();
+                    return ResponseModel.Success(responseData);
+                }
+                return ResponseModel.Error(response.ResponseMessage.ToString());
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errorResponse = await ex.GetResponseJsonAsync<ResponseModel>();
+                return ResponseModel.Error(errorResponse);
+            }
+              
+            } 
+            }
+        }
+    
+   
